185f12fb2555ddbd89f5c7a9e1178a63fe3ba2c1
Dominic <dsal.7k@gmail.com>
2023-02-16 01:32:32 +0800

    prep pizza for holy nuke

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # pytoodo
-a Todo List CLI application written in Python
+
+pytoodo is a Todo List CLI application written in Python.
+It allows users to manage their daily tasks and
+keep track of their progress in a terminal window.
+This terminal CLI app is inspired by the [**Study Together**](https://discord.gg/study) discord server.
+
+[![asciicast](https://asciinema.org/a/PBUJJsmfQP1JgD8Jqdl2dtim0.svg)](https://asciinema.org/a/PBUJJsmfQP1JgD8Jqdl2dtim0)
+
+## Features
+
+- **Stay on track!** Add tasks as needed. Delete tasks when you're done.
+- Your tasks are persistent **in-between** sessions! The app can remember past tasks even after you closed it!
+- It **just works** and **respects your privacy**.
+- **This CLI app** does not share data to 3rd-party companies. All user data-stays on your device.
+- **Free and Open Source**. As it should be.
+
+## Requirements
+
+- Python 3.x
+
+## Installation
+
+```console
+# clone the repo
+$ git clone https://github.com/sherlock-project/sherlock.git
+
+# change the working directory to sherlock
+$ cd sherlock
+
+# install the requirements
+$ python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+```console
+$ python3 pytoodo.py
+pytoodo / your tasks (4)
+-------------------------
+1. water the plants (living room)
+2. pay karl a visit at the bulding (2F, Unit 3)
+3. dispose the garbage bin before going out
+4. get ready for church (0430H)
+```
---
+++ pytoodo.py
@@ -0,0 +1,141 @@
+import os
+import platform
+import sys
+
+# Functions
+def display_line_art(title_bar_msg: str) -> str:    
+    # 1: >>--------------->
+    # 2: :.:.:.:.:.:.:.:.:.:.:
+    # 3: @-->--->--->--->--->
+    # 4: ------------------
+    
+    # set style[1-4]:
+    style = 4
+    
+    # len of header
+    number = 25
+    
+    if style == 1:
+        string = f">>" + "-"*number + ">"    
+    elif style == 2:
+        string = f":."*number + ":"
+    elif style == 3:
+        string = f"@--" + ">---"*number + ">"
+    elif style == 4:
+        string = f"-"*number
+    else:
+        string = []
+    return title_bar_msg + "\n" + string
+
+def display_tasks():
+    if not os.path.exists("tasks.txt") or os.stat("tasks.txt").st_size == 0:
+        print("You have no tasks for today! Try adding some tasks below.")
+        return
+
+    with open("tasks.txt", "r") as file:
+        tasks = file.readlines()
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task.strip()}")
+
+
+def add_task():
+    clear_screen()
+    display_header()
+    display_tasks()
+    task = input("\nEnter your task:\n> ")
+    with open("tasks.txt", "a") as file:
+        file.write(f"{task}\n")
+    print("Task added successfully!")
+
+def delete_task():
+    clear_screen()
+    display_header()
+    display_tasks()
+    task_index = False
+    while task_index is False:
+        try:
+            task_index = int(input("\nEnter the number of the task you want to delete: "))
+        except Exception:
+            clear_screen()
+            display_header()
+            display_tasks()
+            continue
+        
+    with open("tasks.txt", "r") as file:
+        tasks = file.readlines()
+    tasks = [task for task in tasks if task.strip()]
+    if task_index < 1 or task_index > len(tasks):
+        print("Invalid task number. Try again.")
+        return
+    with open("tasks.txt", "w") as file:
+        for i, task in enumerate(tasks, 1):
+            if i != task_index:
+                file.write(f"{task}")
+    print("Task deleted successfully!")
+
+
+   
+def display_choices():
+        print("\nChoices: ")
+        print("[1] - Show all tasks")
+        print("[2] - Add task")
+        print("[3] - Delete task")
+        print("[q] - Quit")
+
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def count_newlines(file_path):
+    if not os.path.exists("tasks.txt") or os.stat("tasks.txt").st_size == 0:
+        return 0
+    with open(file_path, 'r') as f:
+        contents = f.read()
+        return contents.count('\n')
+
+def display_header():
+    file_name = "tasks.txt"
+    lines = count_newlines(file_name)
+    print(display_line_art(f"pytoodo / your tasks ({lines})"))
+
+def dislay_main_menu():
+        clear_screen()
+        display_header()    
+        display_tasks()
+        
+# Main program
+
+
+def main():
+    while True:
+        dislay_main_menu()
+        
+        # Ask and get user input
+        display_choices()
+        choice = input("Enter your choice [1-3, q(uit)]\n> ")
+        
+        # Process user input
+        # [1] - Show all tasks
+        if choice == "1":
+            clear_screen()
+            display_header()
+            display_tasks()
+
+        # [2] - Add task
+        elif choice == "2":
+            add_task()
+        # [3] - Delete task
+        elif choice == "3":
+            delete_task()
+        # [q] - Quit
+        elif choice.upper() == "Q":
+            clear_screen()
+            sys.exit()
+        else:
+            display_header()
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main()
